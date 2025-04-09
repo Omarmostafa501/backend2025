4a2c0fca0ef0cf5ffcd4d7ff2db0d048baf9efe0
Omarmostafa501 <omarmostafa60511@gmail.com>
2025-04-09 16:17:03 +0200

    Firs Deploy

---
+++ omar/index.php
@@ -0,0 +1,21 @@
+<?php
+$name = "omar"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <h1><?php print($name)?></h1>
+  <h1>omar</h1>
+  <h2>mahran</h2>
+  <h3>omar</h3>
+  
+  
+</body>
+</html>
