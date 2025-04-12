19a13db315357a779fbdb0bf377a8fb60954af8b
Omarmostafa501 <omarmostafa60511@gmail.com>
2025-04-12 15:46:47 +0200

    Add files via upload

---
+++ second.php
@@ -0,0 +1,22 @@
+<?php
+$number1 = 10; 
+$number2 = 20; 
+
+$sum = $number1 + $number2;
+
+echo "The sum of $number1 + $number2 is: $sum";
+
+
+$a = 4;
+
+echo "I have " . ($a++) . " books<br>"; 
+echo "Now I have " . $a . " books"; 
+ 
+$sentence = null;
+$first = "In";
+$second = "PHP";
+$third = "Course";
+echo  $first.$sentence.$second.$third;
+
+?>
+
