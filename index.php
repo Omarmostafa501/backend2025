e1d9e25cfb858315311533df8113505095c5e7c8
Omarmostafa501 <omarmostafa60511@gmail.com>
2025-04-19 13:55:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,45 @@
 <?php
-$name = "omar"
-?>
+function create_employees() {
+    $employees = array(
+        array(
+            "name" => "amr",
+            "salary" => 50000,
+            "age" => 30,
+            "insurance" => "Yes"
+        ),
+        array(
+            "name" => "mohamed",
+            "salary" => 60000,
+            "age" => 28,
+            "insurance" => "No"
+        ),
+        array(
+            "name" => "ahmed",
+            "salary" => 70000,
+            "age" => 45,
+            "insurance" => "Yes"
+        )
+    );
+    return $employees;
+}
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-  <h1><?php print($name)?></h1>
-  <h1>omar</h1>
-  <h2>mahran</h2>
-  <h3>omar</h3>
-  
-  
-</body>
-</html>
+
+function display_employees($employees) {
+    $html = "<div class='container'>";
+    foreach ($employees as $employee) {
+        $html .= "<div class='employee-container'>";
+        $html .= "<div class='label'>Name: </div><div class='value'>{$employee['name']}</div>";
+        $html .= "<div class='label'>Salary: </div><div class='value'>\${$employee['salary']}</div>";
+        $html .= "<div class='label'>Age: </div><div class='value'>{$employee['age']}</div>";
+        $html .= "<div class='label'>Insurance: </div><div class='value'>{$employee['insurance']}</div>";
+        $html .= "</div>";
+    }
+    $html .= "</div>";
+    return $html;
+}
+
+
+$employees = create_employees();
+$html_output = display_employees($employees);
+echo $html_output;
+?>
